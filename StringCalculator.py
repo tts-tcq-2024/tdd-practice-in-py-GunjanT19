e0f5a528068197a0d5cd85b9f0aa991b91a8afe4
GunjanT19 <78368508+GunjanT19@users.noreply.github.com>
2024-07-15 14:56:11 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,19 +1,35 @@
-class StringCalculator:
-    @staticmethod
-    def add(numbers: str) -> int:
-        if numbers == "":
-            return 0
-        
-        delimiter = ","
-        if numbers.startswith("//"):
-            custom_delimiter, numbers = numbers.split("\n", 1)
-            delimiter = custom_delimiter[2:]
-        
-        numbers = numbers.replace("\n", delimiter)
-        numbers_list = numbers.split(delimiter)
-        
-        negatives = [int(num) for num in numbers_list if num.startswith('-')]
-        if negatives:
-            raise Exception(f"Negatives not allowed: {', '.join(map(str, negatives))}")
-        
-        return sum(int(num) for num in numbers_list if int(num) <= 1000)
+import re
+def add(var1): 
+    if var1 == "" or var1 == "0":
+        return 0
+    delimiter = delimiter_call(var1)
+    var2=numbers(var1,delimiter)
+    result=sumnumbers(var2)
+    return result
+def delimiter_call(var1):
+     if var1.startswith("//"):
+         return var1[2]
+     return ','
+def numbers(var1,delimiter):
+     if var1.startswith("//"):
+         return var1[4:].split(delimiter)
+     return re.split(rf"{re.escape(delimiter)}|\n", var1)
+def sumnumbers(var2):
+    return sum(parse_int(num) for num in var2 if valid_number(num))
+def parse_int(num_str):
+    try:
+        return int(num_str)
+    except ValueError:
+        return 0
+def valid_number(num_str):
+    try:
+        num=int(num_str)
+        return num<=1000
+    except ValueError:
+        return False
+    
+   
+
+    
+    
+    
